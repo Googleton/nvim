1092218eb69f6a1e91ce1953b4dda66128096875
Googleton <hugo.viallon.pro@gmail.com>
2022-07-29 00:40:20 +0200

    Add git cmd because lazy

--- lua/hv/remap.lua
+++ lua/hv/remap.lua
@@ -6,7 +6,8 @@ nnoremap("<leader>w", "<cmd>w<CR>") -- Shortcut save... may also remove
 
 nnoremap("<leader>ff", "<cmd>Telescope git_files<CR>") -- I use git files instead of find files cuz it avoids .gitignored stuff and .git folder :) 
 nnoremap("<leader>gf", "<cmd>Telescope find_files<CR>") -- In the unlikely case I'm outside of a git repo...
-nnoremap("<leader>fb", "<cmd>Telescope file_browser<CR>") 
+nnoremap("<leader>fb", "<cmd>Telescope file_browser<CR>")
+nnoremap("<leader>gg", "<cmd>Git<CR>")
 
 nnoremap("<leader>h", "<cmd>tabp<CR>")
 nnoremap("<leader>l", "<cmd>tabn<CR>")
