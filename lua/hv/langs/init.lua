a0da32cdaa09563c9eb941c63a22ec66fc13e479
Googleton <hugo.viallon.pro@gmail.com>
2022-07-28 13:24:53 +0200

    Reenable CMP and more

---
+++ lua/hv/langs/init.lua
@@ -0,0 +1,29 @@
+-- This is shamelessly copied from something I saw online lmfao
+
+-- How it works:
+-- Each language specified here will register B and E commands to Build and Execute
+-- It will call whatever command is in build and exec
+local lang_maps = {
+    rust = { exec = "cargo run" },
+    --TODO: Probably add other languages
+}
+
+for lang, data in pairs(lang_maps) do 
+    if data.build ~= nil then
+        vim.api.nvim_create_autocmd(
+            "FileType",
+            { command = "nnoremap <Leader>b :!" .. data.build .. "<CR>", pattern = lang}
+        )
+    end
+
+    vim.api.nvim_create_autocmd(
+        "FileType",
+        { command = "nnoremap <Leader>e :split<CR>:ter " .. data.exec .. "<CR>", pattern = lang }
+    )
+end
+
+-- autoformat the file on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+    command = "lua vim.lsp.buf.formatting_sync(nil, 1000)",
+    pattern = "*.lua,*.rs"
+})
--- lua/init.lua
+++ lua/init.lua
@@ -1,6 +1,6 @@
-local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim" 
+local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then 
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
 end
 
@@ -11,47 +11,47 @@ vim.api.nvim_create_autocmd(
 )
 
 require("packer").startup(function(use)
-  use "wbthomason/packer.nvim" 
+    use "wbthomason/packer.nvim"
 
-  -- Theming 
-  use "folke/tokyonight.nvim"
-  use "rakr/vim-one"
+    -- Theming
+    use "folke/tokyonight.nvim"
+    use "rakr/vim-one"
 
-  -- LSP & Languages 
-  use "neovim/nvim-lspconfig"
-  use "williamboman/nvim-lsp-installer" 
-  use "tamago324/nlsp-settings.nvim"
+    -- LSP & Languages
+    use "neovim/nvim-lspconfig"
+    use "williamboman/nvim-lsp-installer"
+    use "tamago324/nlsp-settings.nvim"
 
-  -- CMP 
-  use "hrsh7th/nvim-cmp"
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/cmp-path"
-  use "hrsh7th/cmp-cmdline"
-  use "hrsh7th/cmp-vsnip"
-  use "hrsh7th/cmp-nvim-lsp"
+    -- CMP
+    use "hrsh7th/nvim-cmp"
+    use "hrsh7th/cmp-buffer"
+    use "hrsh7th/cmp-path"
+    use "hrsh7th/cmp-cmdline"
+    use "hrsh7th/cmp-vsnip"
+    use "hrsh7th/cmp-nvim-lsp"
+    use "windwp/nvim-autopairs"
+    -- Snippets
+    use "saadparwaiz1/cmp_luasnip"
+    use "L3MON4D3/LuaSnip"
+    use "rafamadriz/friendly-snippets"
 
-  -- Snippets
-  use "saadparwaiz1/cmp_luasnip"
-  use "L3MON4D3/LuaSnip"
-  use "rafamadriz/friendly-snippets"
+    -- Treesitter
+    use "nvim-treesitter/nvim-treesitter"
 
-  -- Treesitter
-  use "nvim-treesitter/nvim-treesitter"
+    -- Rust specific
+    use "simrat39/rust-tools.nvim"
 
-  -- Rust specific
-  use "simrat39/rust-tools.nvim"
+    -- UI Improvements
+    use "nvim-telescope/telescope.nvim"
+    use "nvim-lua/plenary.nvim"
+    use "nvim-telescope/telescope-ui-select.nvim"
+    use "nvim-telescope/telescope-file-browser.nvim"
+    use { "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } }
 
-  -- UI Improvements
-  use "nvim-telescope/telescope.nvim"
-  use "nvim-lua/plenary.nvim"
-  use "nvim-telescope/telescope-ui-select.nvim"
-  use "nvim-telescope/telescope-file-browser.nvim"
-  use { "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true }}
-
-  -- Git 
-  use "tpope/vim-fugitive"
-  use "lewis6991/gitsigns.nvim"
-  use "Pocco81/AutoSave.nvim"
+    -- Git
+    use "tpope/vim-fugitive"
+    use "lewis6991/gitsigns.nvim"
+    use "Pocco81/AutoSave.nvim"
 end)
 
 
@@ -65,4 +65,15 @@ require("hv/remap")
 -- UI Related stuff
 require("hv/ui")
 
+-- LSP Related stuff
 require("hv/lsp")
+
+-- Autocompletion
+require("hv/cmp")
+-- TODO: Treesitter probably needs to get enabled
+-- Languages
+require("hv/langs")
+
+
+-- Other manual configs that would just be oneliners elsewhere
+require("nvim-autopairs").setup()
